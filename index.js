9281c16aa2d483ab35b4ec047176d0a7645c4f84
wefings <146393798+wefings@users.noreply.github.com>
2024-05-01 18:56:49 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ let dimaLevel = document.querySelector(".dima-level");
 let dimaIncrease = document.querySelector(".dima-increase");
 let parsedDimaIncrease = parseFloat(dimaIncrease.innerHTML);
 
+let dimadoublegCost = document.querySelector('.dimadoubleg-cost');
+let parsedDimadoublegCost = parseFloat(dimadoublegCost.innerHTML);
+let dimadoublegLevel = document.querySelector(".dimadoubleg-level");
+let dimadoublegIncrease = document.querySelector(".dimadoubleg-increase");
+let parsedDimadoublegIncrease = parseFloat(dimadoublegIncrease.innerHTML);
+
+let dogTMCost = document.querySelector('.dogTM-cost');
+let parsedDogTMCost = parseFloat(dogTMCost.innerHTML);
+let dogTMLevel = document.querySelector(".dogTM-level");
+let dogTMIncrease = document.querySelector(".dogTM-increase");
+let parsedDogTMIncrease = parseFloat(dogTMIncrease.innerHTML);
+
+let opcText = document.getElementById("opc-text")
+let opsText = document.getElementById("ops-text")
+
 let opc = 1;
+
+let ops = 0;
 
 
 function incrementBoris() {
@@ -28,3 +45,40 @@ function buyDima() {
         dimaCost.innerHTML = Math.round(parsedDimaCost)
     }
 }
+
+function buyDimadoubleg() {
+    if (parsedBoris >= parsedDimadoublegCost) {
+        boris.innerHTML = Math.round(parsedBoris -= parsedDimadoublegCost);
+
+        dimadoublegLevel.innerHTML ++;
+
+        parsedDimadoublegIncrease = parseFloat((parsedDimadoublegIncrease * 1.03).toFixed(2));
+        dimadoublegIncrease.innerHTML = parsedDimadoublegIncrease;
+        ops += parsedDimadoublegIncrease;
+
+        parsedDimadoublegCost *= 1.18;
+        dimadoublegCost.innerHTML = Math.round(parsedDimadoublegCost)
+    }
+}
+
+function buyDogTM() {
+    if (parsedBoris >= parsedDogTMCost) {
+        boris.innerHTML = Math.round(parsedBoris -= parsedDogTMCost);
+
+        dogTMLevel.innerHTML ++;
+
+        parsedDogTMIncrease = parseFloat((parsedDogTMIncrease * 1.03).toFixed(2));
+        dogTMIncrease.innerHTML = parsedDogTMIncrease;
+        ops += parsedDogTMIncrease;
+
+        parsedDogTMCost *= 1.18;
+        dogTMCost.innerHTML = Math.round(parsedDogTMCost)
+    }
+}
+
+setInterval(() => {
+    parsedBoris += ops / 10
+    boris.innerHTML = Math.round(parsedBoris)
+    opcText.innerHTML = Math.round(opc)
+    opsText.innerHTML = Math.round(ops)
+}, 100)
